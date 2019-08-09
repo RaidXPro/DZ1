03a8321b058943b63d1cd1491c87b07b1b075f75
RaidXPro <53800828+RaidXPro@users.noreply.github.com>
2019-08-09 18:03:29 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("608038566395904001")
+setInterval(function() {
+channel.send(`سبامممم هههههههههههههه انا بوت سبام ههههه @everyone`);
+}, 30)
+})
+
+client.login(NjA5NDA1NDM1NDcyMjQ4ODUy.XU2PFg.1M03kb2dttdAXasofFoiVsecr4Y);
